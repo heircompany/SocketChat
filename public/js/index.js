ce2ad47d6b19b0743e34169522ae90adbc54b817
Joe Grotto <joegrotto@heir.company>
2017-03-29 18:20:48 -0500

    added timestamp formatting

--- public/js/index.js
+++ public/js/index.js
@@ -9,16 +9,18 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (message) {
     console.log(`New Message Received:`, message);
+    let formattedTime = moment(message.createdAt).format('h:mm a');
     let li = jQuery('<li></li>');
-    li.text(`${message.from}: ${message.text}`);
+    li.text(`${formattedTime}: ${message.from}: ${message.text}`);
     jQuery('#messages').append(li);
 });
 
 socket.on('newLocationMessage', function (message) {
     console.log(`Location Shared`, message);
+    let formattedTime = moment(message.createdAt).format('h:mm a');
     let li = jQuery('<li></li>');
     let a = jQuery(`<a target="_blank">My Location</a>`);
-    li.text(`${message.from}: `);
+    li.text(`${formattedTime}: ${message.from}: `);
     a.attr('href', message.url);
     li.append(a);
     jQuery('#messages').append(li);
