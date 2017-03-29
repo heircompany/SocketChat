8e0c623161a5e5154d0b80c3bf95ab66523cc0a3
Joe Grotto <joegrotto@heir.company>
2017-03-29 03:24:39 -0500

    added styling and more

--- public/js/index.js
+++ public/js/index.js
@@ -26,10 +26,14 @@ socket.on('newLocationMessage', function (message) {
 
 jQuery('#message-box').on('submit', function (e) {
     e.preventDefault();
+
+    let messageTextbox = jQuery('[name=message]');
+
     socket.emit(`createMessage`, {
         from: `User`,
-        text: jQuery('[name=message]').val()
+        text: messageTextbox.val()
         }, function () {
+            messageTextbox.val('')
     });
 });
 
@@ -38,12 +42,17 @@ locationButton.on('click', function () {
     if (!navigator.geolocation) {
         return alert('Geolocation not supported by your browser.');
     };
+
+    locationButton.attr('disabled', 'disabled').text('Sharing location...');
+
     navigator.geolocation.getCurrentPosition(function (position) {
+        locationButton.removeAttr('disabled').text('Share location');
         socket.emit(`createLocationMessage`, {
             latitude:  position.coords.latitude,
             longitude: position.coords.longitude
         });
     }, function () {
+        locationButton.removeAttr('disabled').text('Share location');
         alert(`Cannot fetch location without permission!`);
     });
 });
--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ io.on('connection', (socket) => {
     socket.on('createMessage', (message, callback) => {
         console.log(`createMessage`, message);
         io.emit(`newMessage`, generateMessage(message.from, message.text));
-        callback(`I think you're ghey`);
+        callback();
     });
 
 // LOCATION SHARE
