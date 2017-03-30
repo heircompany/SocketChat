1a683c9e15580ec695810edae2d298b569d22d87
Joe Grotto <joegrotto@heir.company>
2017-03-29 23:37:39 -0500

    added mustachejs templates

--- public/js/index.js
+++ public/js/index.js
@@ -10,20 +10,27 @@ socket.on('disconnect', function () {
 socket.on('newMessage', function (message) {
     console.log(`New Message Received:`, message);
     let formattedTime = moment(message.createdAt).format('h:mm a');
-    let li = jQuery('<li></li>');
-    li.text(`${formattedTime}: ${message.from}: ${message.text}`);
-    jQuery('#messages').append(li);
+    let template = jQuery('#message-template').html();
+    let html = Mustache.render(template, {
+        text: message.text,
+        from: message.from,
+        createdAt: formattedTime
+    });
+
+    jQuery('#messages').append(html);
 });
 
 socket.on('newLocationMessage', function (message) {
     console.log(`Location Shared`, message);
     let formattedTime = moment(message.createdAt).format('h:mm a');
-    let li = jQuery('<li></li>');
-    let a = jQuery(`<a target="_blank">My Location</a>`);
-    li.text(`${formattedTime}: ${message.from}: `);
-    a.attr('href', message.url);
-    li.append(a);
-    jQuery('#messages').append(li);
+    let template = jQuery('#location-message-template').html();
+    let html = Mustache.render(template, {
+        url: message.url,
+        from: message.from,
+        createdAt: formattedTime
+    });
+
+    jQuery('#messages').append(html);
 });
 
 jQuery('#message-box').on('submit', function (e) {
